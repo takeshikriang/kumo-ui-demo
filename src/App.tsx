5247d253cd6902aba6403bbc05b681ef59fbdc58
takeshikriang <contact@kriang.com>
2026-02-16 17:18:08 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Example } from "./components/example";
+
+function App() {
+  return (
+    <div className="container p-4">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/example.tsx
@@ -0,0 +1,9 @@
+export function Example() {
+  return (
+    <div className="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut adipisci
+      repellat mollitia vero ullam esse qui totam, hic enim, et nobis earum
+      molestias delectus officiis quae, obcaecati quod maxime?
+    </div>
+  );
+}
